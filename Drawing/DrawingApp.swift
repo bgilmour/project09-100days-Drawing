c2b1bdbf7a5aa8d533ec4af284a2a9f8793dc688
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-13 01:17:03 +0100

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Bruce Gilmour on 2021-07-13.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

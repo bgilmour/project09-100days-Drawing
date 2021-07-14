dac1589fcce92a2a05b80f1d7fa259676d5f32d7
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-15 00:25:03 +0100

    Creating a spirograph with SwiftUI

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,7 +9,96 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CheckerboardView()
+        SpirographView()
+    }
+}
+
+struct SpirographView: View {
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    @State private var hue = 0.6
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                .frame(width: 300, height: 300)
+
+            Spacer()
+
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10 ... 150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Outer radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10 ... 150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 1 ... 150, step: 1)
+                    .padding([.horizontal, .bottom])
+
+                Text("Amount: \(amount, specifier: "%.2f")")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+
+                Text("Color: \(hue, specifier: "%.2f")")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        let outerRadius = CGFloat(self.outerRadius)
+        let innerRadius = CGFloat(self.innerRadius)
+        let distance = CGFloat(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(divisor)) * amount
+
+        var path = Path()
+
+        for theta in stride(from: 0, to: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+
+            x += rect.width / 2
+            y += rect.height / 2
+
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+
+        return path
+    }
+
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+
+        return a
     }
 }
 
@@ -28,43 +117,6 @@ struct CheckerboardView: View {
     }
 }
 
-struct Checkerboard: Shape {
-    var rows: Int
-    var columns: Int
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        let rowSize = rect.height / CGFloat(rows)
-        let columnSize = rect.width / CGFloat(columns)
-
-        for row in 0 ..< rows {
-            for column in 0 ..< columns {
-                if (row + column).isMultiple(of: 2) {
-                    let startX = columnSize * CGFloat(column)
-                    let startY = rowSize * CGFloat(row)
-
-                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-                    path.addRect(rect)
-                }
-            }
-        }
-
-        return path
-    }
-
-    var animatableData: AnimatablePair<Double, Double> {
-        get {
-            AnimatableData(Double(rows), Double(columns))
-        }
-
-        set {
-            rows = Int(newValue.first)
-            columns = Int(newValue.second)
-        }
-    }
-}
-
 struct TrapezoidView: View {
     @State private var insetAmount: CGFloat = 50
 
@@ -76,27 +128,6 @@ struct TrapezoidView: View {
                     insetAmount = CGFloat.random(in: 10 ... 90)
                 }
             }
-    }
-}
-
-struct Trapezoid: Shape {
-    var insetAmount: CGFloat
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
-
-        return path
-    }
-
-    var animatableData: CGFloat {
-        get { insetAmount }
-        set { insetAmount = newValue }
     }
 }
 
@@ -498,6 +529,64 @@ struct Flower: Shape {
         }
 
         return path
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+    }
+
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { insetAmount = newValue }
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        for row in 0 ..< rows {
+            for column in 0 ..< columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatableData(Double(rows), Double(columns))
+        }
+
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
     }
 }
 

275cb255cfdc98cb181a61080c7af127fbb50e87
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-14 21:50:54 +0100

    Animating simple shapes with animatableData

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        SaturationBlurView()
+        TrapezoidView()
+    }
+}
+
+struct TrapezoidView: View {
+    @State private var insetAmount: CGFloat = 50
+
+    var body: some View {
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 100)
+            .onTapGesture {
+                withAnimation {
+                    insetAmount = CGFloat.random(in: 10 ... 90)
+                }
+            }
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+
+        return path
+    }
+
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { insetAmount = newValue }
     }
 }
 

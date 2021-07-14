1f5909547c2052d6a25241a809f793c39cbd1b36
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-14 22:32:01 +0100

    Animating complex shapes with AnimatablePair

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,7 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TrapezoidView()
+        CheckerboardView()
+    }
+}
+
+struct CheckerboardView: View {
+    @State private var rows = 4
+    @State private var columns = 4
+
+    var body: some View {
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
+                }
+            }
+    }
+}
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+
+        for row in 0 ..< rows {
+            for column in 0 ..< columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatableData(Double(rows), Double(columns))
+        }
+
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
     }
 }
 
